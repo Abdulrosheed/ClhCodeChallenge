e8cc16fb945c45696f171ecaeb854f47d03d9404
abdulrasheed <ayomideabdulrosheed12345@gmail.com>
2021-12-24 13:27:44 +0100

    CodeChallenge Solution

---
+++ Question 1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CollectNumbers();
+        }
+        static void CollectNumbers ()
+        {
+            
+            int check =0;
+           int count = 0;
+            Console.WriteLine("How many numbers did you want to enter");
+            int size = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Enter your target number");
+            int number = int.Parse(Console.ReadLine());
+
+            int [] numbers = new int [size];
+            for (int i = 0; i < numbers.Length ; i++)
+            {
+                Console.WriteLine("Enter your element");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            
+          
+            
+
+            
+            for(int j = 0; j < numbers.Length ; j++)
+            {
+               
+                if(numbers[j] == number && count == 0)
+                {
+                    
+                    
+                    Console.Write(j);
+                    count++;
+                    
+                    
+                }
+                if(numbers[j] == number)
+                {
+                    check = j;
+                   if(j == numbers.Length - 1)
+                   {
+                        Console.WriteLine($"  , {check}");
+                    }
+                }
+                
+                
+    
+            }
+            if(count <= 0)
+            {
+                Console.WriteLine("-1 , -1");
+            }
+          
+
+        }
+        
+    }
+}
---
+++ Question3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your word");
+            string word = Console.ReadLine();
+            string reset = "";
+            string reset2 = "";
+            int count = 0;
+            int count2 = 0;
+            int min = 0;
+            foreach(var item in word)
+            {
+                count++;
+                for(int k = 0; k < count ; k++)
+                {
+                    reset += item;
+                    if(count > 1)
+                    {
+                    
+                        for(int i = 0; i < reset.Length; i++)
+                        {
+                            if(reset[i] == reset[reset.Length - i - 1])
+                            {
+                                
+                                count2++;
+                            }
+                        }
+                        foreach(var character in reset )
+                        {
+                            if(count2 == reset.Length)
+                            {
+                                reset2 += character; 
+                            }
+                          
+                        }
+                            
+                        for (int j = 1; j > 0 ; j++ )
+                        {
+                            if(j == 1)
+                            {
+                                min = count2;
+                            }
+                            if(j > 1 && count2 <= min )
+                            {
+                                Console.WriteLine(reset2);
+                                break;
+                            }
+                            
+                        }
+                }
+                }
+                
+            }
+            
+        }
+    }
+}
