bbce268c91f15f328caa395e9d4137cb09f247bf
abdulrasheed <ayomideabdulrosheed12345@gmail.com>
2022-03-25 13:01:54 +0100

    CodeChallenge10

---
+++ CodeChallenge10/Question1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckSherlock("aaaaaaaab");
+        }
+        public static void CheckSherlock(string words)
+        {
+            List<int> counts = new List<int>();
+            List<string> numbers = new List<string>();
+
+
+            int count = 1;
+            int count2 = 0;
+            int count3 = 1;
+            int mostFrequentlyOccured = 0;
+        
+            for (int j = 0; j < words.Length; j++)
+            {
+
+                for (int i = j + 1; i < words.Length; i++)
+                {
+                    
+                    
+                
+                    if (words[j] == words[i])
+                    {
+                        count++;
+                    }
+                      
+                
+
+                }
+                if(numbers.Contains(words[j].ToString()))
+                {
+
+                }
+                else
+                {
+                    counts.Add(count);
+                    numbers.Add(words[j].ToString());
+                }
+                
+                count = 1;
+
+            }
+            if(counts.Count == 0)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+            mostFrequentlyOccured = counts[0];
+        
+            for(int l = 0; l < counts.Count; l++)
+            {
+                for(int t = l+1 ; t < counts.Count; t++)
+                {
+                    if(counts[l] == counts[t])
+                    {
+                        count3++;
+                    }
+                }
+                if(count3 > mostFrequentlyOccured)
+                {
+                    mostFrequentlyOccured = counts[l];
+                }
+                count3 = 1;
+                
+            }
+            Console.WriteLine(mostFrequentlyOccured);
+            for(int k = 0; k < counts.Count; k++)
+            {
+                if(mostFrequentlyOccured == counts[k])
+                {
+                    count2++;
+                }
+            }
+            if(count2 == counts.Count)
+            {
+                Console.WriteLine("Yes");
+                return;
+            }
+            else if(counts.Count - count2 == 1)
+            {
+                
+                Console.WriteLine("Yes");
+                return;
+            }
+            else{
+                Console.WriteLine("No");
+                return;
+            }
+        
+
+        }
+    }
+}
---
+++ CodeChallenge10/Question3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            checkFriend(2, 5);
+        }
+        static void checkFriend(int k, int n)
+        {
+            int count = 0;
+            int count2 = 0;
+            int lastIndex = 0;
+            int x = 0;
+            List<string> numbers2 = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                x++;
+                numbers2.Add(x.ToString());
+            }
+                    Console.WriteLine(numbers2.Count);
+            Console.WriteLine($"{String.Join(',', numbers2)}");
+            for (int m = 0; m < -1; m++)
+            {
+                count++;
+                if (count == k)
+                {
+                    numbers2.Remove(numbers2[m]);
+                    count = 1;
+                    lastIndex = m + 1;
+                }
+                count2++;
+        
+                if(count < numbers2.Count)
+                {
+                    var o = numbers2[numbers2.Count - 1];
+                    numbers2.Remove(numbers2[numbers2.Count - 1]);
+                    numbers2.Insert(0,o);
+                    
+                Console.WriteLine($"{String.Join(',', numbers2)}");
+           
+                }
+                if(numbers2.Count == 1)
+                {
+                    break;
+                }
+              
+            }
+                Console.WriteLine($"{String.Join(',', numbers2)}");
+           
+
+        }
+
+
+    }
+}
